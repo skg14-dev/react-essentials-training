c5ff491bec1a461a1bed344a74171b220b40e20c
skg14-dev <skg014@outlook.com>
2022-07-26 15:33:27 +0400

    useEffect and useState

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [emotion,setEmotion] = useState("happy");
+  useEffect(()=> {
+
+    console.log(`Its ${emotion}`);
+  },[emotion]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Current emotion is {emotion}</h1>
+      <button onClick={() => setEmotion("sad")}>sad</button>
     </div>
   );
 }
