fab99187625e820d13fd3a67fc983f421b9794ad
skg14-dev <skg014@outlook.com>
2022-07-26 16:09:30 +0400

    handling forms with UseRef

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
-import { useState, useEffect, useReducer } from "react";
+import { useRef } from "react";
 
 function App() {
-  const [checked, setChecked] = useReducer((checked) => !checked, false);
+  const txtTitle = useRef();
+  const hexColor = useRef();
+  const submit = (e) => {
+    e.preventDefault();
+    const title = txtTitle.current.value;
+    const color = hexColor.current.value;
+    alert(`${title}, ${color}`)
+    txtTitle.current.value = "";
+    hexColor.current.value = "";
+  };
   return (
-    <div className='App'>
-      <input type="checkbox" value={checked} onChange={setChecked} />
-      <label>{checked ? "checked":"unchecked"}</label>
-    </div>
+    <form onSubmit={submit}>
+      <input ref={txtTitle} type="text" placeholder='color title ..'>
+      </input>
+      <input type="color" ref={hexColor}></input>
+      <button>ADD</button>
+    </form>
   );
 }
 
