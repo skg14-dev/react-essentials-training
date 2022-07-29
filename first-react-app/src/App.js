6e033164d9f31f51a33fc9148e08fcc198df592a
skg14-dev <skg014@outlook.com>
2022-07-29 21:11:47 +0400

    fetching data with hooks

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,34 +1,16 @@
 import './App.css';
-import { useState } from "react";
-
-function useInput(initialValue) {
-  const [value, setValue] = useState(initialValue);
-  return [
-  {
-    value, 
-    onChange: (e) => setValue(e.target.value)
-  },
-  ()=>setValue(initialValue)];
-  
-}
+import { useEffect, useState } from "react";
 
 function App() {
-  const [titleProps, resetTitle] = useInput("");
-  const [colorProps, resetColor] = useInput("#000000");
-  const submit = (e) => {
-    e.preventDefault();
-    alert(`${titleProps.value}, ${colorProps.value}`)
-    resetColor();
-    resetTitle();
-  };
-  return (
-    <form onSubmit={submit}>
-      <input {...titleProps} type="text" placeholder='color title ..'>
-      </input>
-      <input type="color" {...colorProps}></input>
-      <button>ADD</button>
-    </form>
-  );
+  const [data, setData]= useState(null);
+  useEffect(()=>{
+    fetch(`https://api.github.com/users/skg14-dev`)
+    .then((respnse) => respnse.json())
+    .then(data => setData(data));
+  },[]);
+  if(data){
+    return <pre>{JSON.stringify(data,null,2)}</pre>
+  }
 }
 
 export default App;
