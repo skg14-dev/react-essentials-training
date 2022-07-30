b06cece26505134aa532df8b9908d41e11420968
skg14-dev <skg014@outlook.com>
2022-07-30 09:26:44 +0400

    handle loading state

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,15 +1,36 @@
 import './App.css';
 import { useEffect, useState } from "react";
 
+function GitHubUser({username, location, avatar}){
+  return (
+    <div>
+      <h1>{username}</h1>
+      <p>{location}</p>
+      <img src={avatar} alt="avatar" height={150}/>
+    </div>
+  );
+}
+
 function App() {
   const [data, setData]= useState(null);
-  useEffect(()=>{
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
     fetch(`https://api.github.com/users/skg14-dev`)
     .then((respnse) => respnse.json())
-    .then(data => setData(data));
+    .then(setData)
+    .then(() => setLoading(false))
+    .catch(setError);
   },[]);
+  if (error) {
+    return <pre>{JSON.stringify(error)}</pre>
+  }
   if(data){
-    return <pre>{JSON.stringify(data,null,2)}</pre>
+    return <GitHubUser username={data.login} location={data.location} avatar={data.avatar_url}/>
+  }
+  else{
+    return null;
   }
 }
 
