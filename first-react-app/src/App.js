4e3ff65507ba6061714a55013e645ec502612fd4
skg14-dev <skg014@outlook.com>
2022-07-30 10:00:59 +0400

    incorporating link components

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,34 +1,41 @@
-import './App.css';
+import "./App.css";
+import { Link } from "react-router-dom";
 
-export function Home(){
-  return(
+export function Home() {
+  return (
     <div>
+      <nav>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
       <h1>My Website</h1>
     </div>
-  )
+  );
 }
 export function About() {
-  return(
+  return (
     <div>
-    <h1>About us</h1>
-  </div>
-  
-  )
-
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+      <h1>About us</h1>
+    </div>
+  );
 }
 
 export function Contact() {
-  return(
+  return (
     <div>
-    <h1>Contact us</h1>
-  </div>
-  )
-
+      <nav>
+        <Link to="/about">About</Link>
+        <Link to="/">Home</Link>
+      </nav>
+      <h1>Contact us</h1>
+    </div>
+  );
 }
 
 export function App() {
-  return(
-    <h1>Hello world</h1>
-  )
+  return <Home />;
 }
-
--- first-react-app/src/index.js
+++ first-react-app/src/index.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import {App,About,Contact} from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter, Routes,Route} from "react-router-dom"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { App, About, Contact } from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App/>}/>
-      <Route path="/about" element={<About/>}/>
-      <Route path="/contact" element={<Contact/>}/>
-    </Routes>
-
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
     </BrowserRouter>
-
   </React.StrictMode>
 );
 
