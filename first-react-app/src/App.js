0792a530d7abfdb0494e166d74447b853665ba00
skg14-dev <skg014@outlook.com>
2022-07-30 09:45:13 +0400

    working with render props

--- first-react-app/src/App.js
+++ first-react-app/src/App.js
@@ -1,74 +1,29 @@
 import './App.css';
 import { useEffect, useState } from "react";
 
-const query = `query { 
-  allLifts {
-    name
-    elevationGain
-    status
-  }
-}`
+const tahoo_peeks = [
+  { name : "Freel", elevation: 1234},
+  { name : "Pyramid", elevation: 5678}
+]
 
-const opts = {
-  method: "POST",
-  headers: {
-    "Content-Type":"application/json"
-  },
-  body: JSON.stringify({query})
-}
-
-function GitHubUser({username, location, avatar}){
-  return (
-    <div>
-      <h1>{username}</h1>
-      <p>{location}</p>
-      <img src={avatar} alt="avatar" height={150}/>
-    </div>
-  );
-}
-
-function Lift({name, elevationGain, status}) {
-  return (
-    <div>
-      <h1>{name}</h1>
-      <p>{elevationGain}</p>
-      <p><b>{status}</b></p>
-    </div>
+function List({data, renderItem, renderEmpty}) {
+  return !data.length ? (renderEmpty) : 
+  (
+    <ul>
+      {data.map((item) => (
+        <li key={item.name}>{renderItem(item)}</li>
+      ))}
+    </ul>
   )
-}
+} 
 
 function App() {
-  const [data, setData]= useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    setLoading(true);
-    // fetch(`https://api.github.com/users/skg14-dev`)
-    fetch(`https://snowtooth.moonhighway.com/`, opts)
-    .then((respnse) => respnse.json())
-    .then(setData)
-    .then(() => setLoading(false))
-    .catch(setError);
-  },[]);
-  if (loading) {
-    return <h1>Loading..</h1>
-  }
-  if (error) {
-    return <pre>{JSON.stringify(error)}</pre>
-  }
-  // if(data){
-  //   return <GitHubUser username={data.login} location={data.location} avatar={data.avatar_url}/>
-  // }  
-  if(data){
-    return <div>
-      {data.data.allLifts.map((lift) => (
-        <Lift name={lift.name} elevationGain={lift.elevationGain} status={lift.status} />
-      ))}
-      </div>
-  }
-  else{
-    return null;
-  }
+  return(
+    <List data={tahoo_peeks} renderEmpty={<p>List is empty</p>} renderItem={(item) =>
+    (<>
+      {item.name} -{item.elevation}
+    </>)} />
+  )
 }
 
 export default App;
